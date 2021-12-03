76e2ea7588f400fba828368d107662d6e96325ab
xbforce <62805453+xbforce@users.noreply.github.com>
2021-12-03 08:30:57 +0000

    Update vpn_monitoring.sh

--- vpn_monitoring.sh
+++ vpn_monitoring.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/bash
 
-ifc=$(ping -c 3 -W 20 -I tun0 8.8.8.8)
-echo "tun0 interface is connected."
+if [[ -z $1 ]]; then
+    printf "[*] twitter.com/xbforce | github.com/xbforce"
+    printf "[*] Usage: $0 <interface>"
+    exit 0
+fi
+
+ifc=$(ping -c 5 -W 10 -I $1 8.8.8.8)
+echo "$1 interface is connected."
 
 if [[ $ifc == *"0 received"* ]]; then
-    DISPLAY=:0.0 notify-send normal -t 20000 "VPN Disconnected!"
+    DISPLAY=:0.0 notify-send -u critical -i /home/kali/Pictures/redcross1.png -t 20000 "VPN Disconnected!"
     sleep 3
     pkill -f "watch -n"
     exit 0
 else
     :
 fi
-
